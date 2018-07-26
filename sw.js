a98cbd4e78c022f6e9985cee007846dca5632234
Jason Arnold <jason.arnold.00@gmail.com>
2018-07-26 16:22:42 -0400

    add line to cache content from leaflet

--- sw.js
+++ sw.js
@@ -7,7 +7,8 @@ self.addEventListener('install', function(e) {
        '/css/styles.css',
        '/js/dbhelper.js',
        '/js/main.js',
-       '/js/restaurant_info.js'
+       '/js/restaurant_info.js',
+       'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
     ]);
    })
  );
